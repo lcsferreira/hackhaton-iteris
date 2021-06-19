d47dacac3b943787c17bf542f125452fccd518e3
Isadora Dornelas Teixeira Silva <isadora1405@gmail.com>
2021-06-19 11:44:26 -0300

    criando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuTemplateComponent } from './components/menu-template/menu-template.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { SobrePageComponent } from './components/sobre-page/sobre-page.component';
+import { InstituicoesPageComponent } from './components/instituicoes-page/instituicoes-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTemplateComponent,
+    HomePageComponent,
+    SobrePageComponent,
+    InstituicoesPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
