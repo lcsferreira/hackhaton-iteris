4c1c766b211fec7845049b25d2dc4c24c18835b1
Isadora Dornelas Teixeira Silva <isadora1405@gmail.com>
2021-06-19 13:25:34 -0300

    Mudanças sobrePage 2

--- src/app/components/sobre-page/sobre-page.component.ts
+++ src/app/components/sobre-page/sobre-page.component.ts
@@ -9,7 +9,7 @@ export class SobrePageComponent implements OnInit {
 
   public logoUrl = 'https://d2v9ipibika81v.cloudfront.net/uploads/sites/32/2016/12/GBV-woman-pt-1140-1140x450.png';
   public titulo = 'Sobre a Página';
-  public descricao = 'Somos uma página de apoio a mulher. Aqui você encontrará informaçóes de instituições públicas dispostas a ajudar. Sinta-se acolhida por nós!';
+  public descricao = 'Somos uma página de apoio a mulher. Aqui você encontrará informações de instituições públicas dispostas a ajudar. Sinta-se acolhida por nós!';
 
   constructor() { }
 
